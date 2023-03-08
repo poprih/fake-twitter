fe2d3771cba95952b7c098c437e16c1fab670602
Riku <xb_lu@manka.co.jp>
2023-03-08 21:42:34 +0900

    fix: method

--- pages/api/initDb.ts
+++ pages/api/initDb.ts
@@ -6,10 +6,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const rawPath = path.join(process.cwd(), 'db')
-  const userJSON = await fs.readFileSync(rawPath + '/user.json', 'utf-8')
-  const tweetJSON = await fs.readFileSync(rawPath + '/tweet.json', 'utf-8')
-  await fs.writeFileSync('/tmp/user.json', userJSON, { encoding: 'utf-8' })
-  await fs.writeFileSync('/tmp/tweet.json', tweetJSON, { encoding: 'utf-8' })
-  res.status(200)
+  if (req.method === 'GET') {
+    const rawPath = path.join(process.cwd(), 'db')
+    const userJSON = await fs.readFileSync(rawPath + '/user.json', 'utf-8')
+    const tweetJSON = await fs.readFileSync(rawPath + '/tweet.json', 'utf-8')
+    await fs.writeFileSync('/tmp/user.json', userJSON, { encoding: 'utf-8' })
+    await fs.writeFileSync('/tmp/tweet.json', tweetJSON, { encoding: 'utf-8' })
+    res.status(200)
+  }
 }
